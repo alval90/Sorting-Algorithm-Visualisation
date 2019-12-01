f680a1a7d5c870ebae4296fc67c35b3873ce41af
Alex <alexg1990@users.noreply.github.com>
2019-12-01 18:44:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -28,32 +28,10 @@ class App extends React.Component {
     this.setState({arr: arr});
   }
 
-  bubbleSort = (arr, index = 0) =>  {
-    if(index === arr.length) {
-      this.state.isSorted ? console.log("Eureka") : this.setState({arr: arr, isSorted: true},
-        () => this.bubbleSort(this.state.arr, 0)  
-      );
-    }
-    if (index < arr.length) {
-      if (arr[index] > arr[index + 1]) {
-        let temp = arr[index];
-        arr[index] = arr[index + 1];
-        arr[index + 1] = temp;
-        this.setState({arr: arr, isSorted: false}, 
-          () => setTimeout(() => this.bubbleSort(this.state.arr, index + 1), 30)
-        );
-      } else {
-        this.setState({arr: arr},
-          () => setTimeout(() => this.bubbleSort(this.state.arr, index + 1), 30)
-        );
-      }
-    }
-  }
-
   handleClick = () => {
     switch (this.state.algorithm) {
       case "Bubble":
-        this.bubbleSort(this.state.arr);
+        bubbleSort.call(this,this.state.arr);
         break;
       case "Insertion":
         break;
@@ -96,5 +74,26 @@ class App extends React.Component {
   }
 }
 
+const bubbleSort = function (arr, index = 0)  {
+  if(index === arr.length) {
+    this.state.isSorted ? console.log("Eureka") : this.setState({arr: arr, isSorted: true},
+      () => bubbleSort.call(this, this.state.arr, 0)  
+    );
+  }
+  if (index < arr.length) {
+    if (arr[index] > arr[index + 1]) {
+      let temp = arr[index];
+      arr[index] = arr[index + 1];
+      arr[index + 1] = temp;
+      this.setState({arr: arr, isSorted: false}, 
+        () => setTimeout(() => bubbleSort.call(this,this.state.arr, index + 1), 30)
+      );
+    } else {
+      this.setState({arr: arr},
+        () => setTimeout(() => bubbleSort.call(this,this.state.arr, index + 1), 30)
+      );
+    }
+  }
+}
 
 export default App;
