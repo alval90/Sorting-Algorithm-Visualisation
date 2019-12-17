dcffdd928cd93f961a9b2cf85f91075e4b0e59d2
Alex <alexg1990@users.noreply.github.com>
2019-12-17 20:11:23 +0100

    added working selection sort

--- src/Algorithms.js
+++ src/Algorithms.js
@@ -21,47 +21,50 @@ const algorithm = {
       }
     }
   },
+  // Planned optimization:
+  // 1) Move break condition to top of recursive function for better readability
+  // 2) Reduce amount of loops by one via skipping the first iteration
+  // 3) Encapsulate color highlighting and value swapping
   selectionSort: function (arr, itemBlockStatus, index = 0, minIndex = 0, counter = 0) {
+    // Check if every itemblock has been considered as current minimum value
     if (index < arr.length) {
-      if (arr[index] < arr[minIndex]) {
+      // Define new minimum, if current item that is being analyzed is smaller than the current minimum
+      // Change status of itemBlocks to highlight in respective color
+      if (arr[index] <= arr[minIndex]) {
+        itemBlockStatus[minIndex] = "default";
         minIndex = index;
+        itemBlockStatus[minIndex] = "analyzed";
+        itemBlockStatus[index + 1] = "analyzed";
         this.setState({arr: arr, itemBlockStatus: itemBlockStatus, isSorted: false},
           () => setTimeout(() => algorithm.selectionSort.call(this, this.state.arr, this.state.itemBlockStatus, index + 1, minIndex, counter), 30)
         );
-      } else if (arr[index] >= arr[minIndex]) {
+      // Move one item ahead, if current item that is being analyzed is not smaller than the current minimum
+      // Change status accordingly to use color highlighting
+      } else if (arr[index] > arr[minIndex]) {
+        itemBlockStatus[index] = "default";
+        itemBlockStatus[index + 1] = "analyzed";
         this.setState({arr: arr, itemBlockStatus: itemBlockStatus},
           () => setTimeout(() => algorithm.selectionSort.call(this, this.state.arr, this.state.itemBlockStatus, index + 1, minIndex, counter), 30)
         );
       }
+    // After going through all items to check for a new minimum, check if items need to be swapped to further sort the array
+    // Swap new minimum with current starting item of the loop - Otherwise, consider array as sorted and fill the itemBlockStatus accordingly for color highlighting.
     } else {
       if (this.state.isSorted) {
-        console.log(counter);
         this.setState({itemBlockStatus: itemBlockStatus.fill("sorted")});
       } else {
-        console.log("swapped");
         let tmp = arr[counter];
         arr[counter] = arr[minIndex];
         arr[minIndex] = tmp;
+        itemBlockStatus[minIndex] = "default";
+        itemBlockStatus[counter] = "sorted";
+        itemBlockStatus[counter + 1] = "analyzed";
         this.setState({arr: arr, itemBlockStatus: itemBlockStatus, isSorted: true},
           () => setTimeout(() => algorithm.selectionSort.call(this, this.state.arr, this.state.itemBlockStatus, counter + 1, counter + 1, counter + 1), 30)
         ); 
       }
     }
   }
-  // selectionSort: function (arr) {
-  //   for (let i = 0; i < arr.length; i++) {
-  //     let minIndex = i;
-  //     for (let j = i; j < arr.length; j++) {
-  //       if(arr[j] < arr[minIndex]) {
-  //         minIndex = j;
-  //       }    
-  //     }
-  //     let tmp = arr[i];
-  //     arr[i] = arr[minIndex];
-  //     arr[minIndex] = tmp;
-  //   }
-  //   return arr;
-  // }
 }
 
 function setItemBlockStatus (itemBlockStatus, index) {
