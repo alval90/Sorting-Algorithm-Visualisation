be7795f20b03e066056d96c6b653de4d9d93e76c
alexg1990 <alexg1990@users.noreply.github.com>
2020-01-10 13:12:09 +0100

    started refactoring selection Sort to use setTimeout instead of setTimeouts callback parameter, standardizing the animation approach across sorting algorithms

--- src/Algorithms.js
+++ src/Algorithms.js
@@ -1,169 +1,134 @@
 const algorithm = {
-  bubbleSort: function(arr, index = 0, counter = 0, isSorted = false) {
-    if (index === arr.length - counter - 1) {
-      setItemStatus(arr, [], [], [index]);
-      if (isSorted) {
-        setItemStatus(arr, [], [], [], true);
-        this.setState({
-          arr: arr,
-          isSorted: true,
-          sorting: false
-        });
-      } else {
-        this.setState(
-          {
-            arr: arr,
-            isSorted: true
-          },
-          () =>
-            algorithm.bubbleSort.call(
-              this,
-              this.state.arr,
-              0,
-              counter + 1,
-              true
-            )
-        );
+  bubbleSort: function (arr) {
+    let isSorted = false;
+    let j = 0;
+    while (!isSorted) {
+      isSorted = true;
+      for (let i = 0; i < arr.length - j - 1; i++) {
+        setItemStatus(arr, [], [i, i + 1]);
+        if (arr[i].height > arr[i + 1].height) {
+          swapItems(arr, i, i + 1);
+          isSorted = false;
+        }
+        queueAnimation.call(this, arr);
+        setItemStatus(arr, [i, i + 1], []);
+        queueAnimation.call(this, arr);
+        algorithm.counter -= 1;
       }
-      return;
+      setItemStatus(arr, [], [], [arr.length - j - 1]);
+      j++;
     }
-    if (index < arr.length) {
-      if (arr[index].height > arr[index + 1].height) {
-        swapItems(arr, index, index + 1);
-        setItemStatus(arr, [index], [index + 1, index + 2]);
-        this.setState(
-          {
-            arr: arr
-          },
-          () =>
-            setTimeout(
-              () =>
-                algorithm.bubbleSort.call(
-                  this,
-                  this.state.arr,
-                  index + 1,
-                  counter,
-                  false
-                ),
-              this.state.speed
-            )
-        );
-        return;
-      } else {
-        setItemStatus(arr, [index], [index + 1, index + 2]);
-        this.setState(
-          {
-            arr: arr
-          },
-          () =>
-            setTimeout(
-              () =>
-                algorithm.bubbleSort.call(
-                  this,
-                  this.state.arr,
-                  index + 1,
-                  counter
-                ),
-              this.state.speed
-            )
-        );
-        return;
-      }
-    }
+    setItemStatus(arr, [], [], [], true);
+    queueAnimation.call(this, arr, true);
   },
-  // Planned optimization:
-  // 1) Move break condition to top of recursive function for better readability
-  // 2) Reduce amount of loops by one via skipping the first iteration
-  // 3) Encapsulate color highlighting and value swapping
-  selectionSort: function(
-    arr,
-    index = 0,
-    minIndex = 0,
-    counter = 0,
-    isSorted = false
-  ) {
-    // Check if every itemblock has been considered as current minimum value
-    if (index < arr.length) {
-      // Define new minimum, if current item that is being analyzed is smaller than the current minimum
-      // Change status of itemBlocks to highlight in respective color
-      if (arr[index].height <= arr[minIndex].height) {
-        setItemStatus(arr, [minIndex], [index, index + 1]);
-        minIndex = index;
-        this.setState(
-          {
-            arr: arr
-          },
-          () =>
-            setTimeout(
-              () =>
-                algorithm.selectionSort.call(
-                  this,
-                  this.state.arr,
-                  index + 1,
-                  minIndex,
-                  counter,
-                  false
-                ),
-              this.state.speed
-            )
-        );
-        // Move one item ahead, if current item that is being analyzed is not smaller than the current minimum
-        // Change status accordingly to use color highlighting
-      } else if (arr[index].height > arr[minIndex].height) {
-        setItemStatus(arr, [index], [index + 1]);
-        this.setState(
-          {
-            arr: arr
-          },
-          () =>
-            setTimeout(
-              () =>
-                algorithm.selectionSort.call(
-                  this,
-                  this.state.arr,
-                  index + 1,
-                  minIndex,
-                  counter
-                ),
-              this.state.speed
-            )
-        );
+  selectionSort: function (arr) {
+    let j = 0,
+      minIndex = 0
+    while (j < arr.length) {
+      for (let i = j, minimum = arr[j].height; i < arr.length; i++) {
+        setItemStatus(arr, [i - 1], [i, minIndex]);
+        queueAnimation.call(this, arr);
+        if (arr[i].height < minimum) {
+          minimum = arr[i].height;
+          setItemStatus(arr, [minIndex]);
+          minIndex = i;
+        }
       }
-      // After going through all items to check for a new minimum, check if items need to be swapped to further sort the array
-      // Swap new minimum with current starting item of the loop - Otherwise, consider array as sorted and fill the itemBlockStatus accordingly for color highlighting.
-    } else {
-      if (isSorted) {
-        setItemStatus(arr, [], [], [], true);
-        this.setState({
-          arr: arr,
-          isSorted: true,
-          sorting: false
-        });
-      } else {
-        swapItems(arr, counter, minIndex);
-        setItemStatus(arr, [minIndex], [counter + 1], [counter]);
-        this.setState(
-          {
-            arr: arr
-          },
-          () =>
-            setTimeout(
-              () =>
-                algorithm.selectionSort.call(
-                  this,
-                  this.state.arr,
-                  counter + 1,
-                  counter + 1,
-                  counter + 1,
-                  true
-                ),
-              this.state.speed
-            )
-        );
-      }
+      swapItems(arr, j, minIndex);
+      setItemStatus(arr, [j, arr.length - 1], [], [j]);
+      j++;
     }
+    setItemStatus(arr, [], [], [], true);
+    queueAnimation.call(this, arr, true);
   },
-  insertionSort: function(arr) {
+
+
+  // function (
+  //   arr,
+  //   index = 0,
+  //   minIndex = 0,
+  //   counter = 0,
+  //   isSorted = false
+  // ) {
+  //   // Check if every itemblock has been considered as current minimum value
+  //   if (index < arr.length) {
+  //     // Define new minimum, if current item that is being analyzed is smaller than the current minimum
+  //     // Change status of itemBlocks to highlight in respective color
+  //     if (arr[index].height <= arr[minIndex].height) {
+  //       setItemStatus(arr, [minIndex], [index, index + 1]);
+  //       minIndex = index;
+  //       this.setState({
+  //           arr: arr
+  //         },
+  //         () =>
+  //         setTimeout(
+  //           () =>
+  //           algorithm.selectionSort.call(
+  //             this,
+  //             this.state.arr,
+  //             index + 1,
+  //             minIndex,
+  //             counter,
+  //             false
+  //           ),
+  //           this.state.speed
+  //         )
+  //       );
+  //       // Move one item ahead, if current item that is being analyzed is not smaller than the current minimum
+  //       // Change status accordingly to use color highlighting
+  //     } else if (arr[index].height > arr[minIndex].height) {
+  //       setItemStatus(arr, [index], [index + 1]);
+  //       this.setState({
+  //           arr: arr
+  //         },
+  //         () =>
+  //         setTimeout(
+  //           () =>
+  //           algorithm.selectionSort.call(
+  //             this,
+  //             this.state.arr,
+  //             index + 1,
+  //             minIndex,
+  //             counter
+  //           ),
+  //           this.state.speed
+  //         )
+  //       );
+  //     }
+  //     // After going through all items to check for a new minimum, check if items need to be swapped to further sort the array
+  //     // Swap new minimum with current starting item of the loop - Otherwise, consider array as sorted and fill the itemBlockStatus accordingly for color highlighting.
+  //   } else {
+  //     if (isSorted) {
+  //       setItemStatus(arr, [], [], [], true);
+  //       this.setState({
+  //         arr: arr,
+  //         isSorted: true,
+  //         sorting: false
+  //       });
+  //     } else {
+  //       swapItems(arr, counter, minIndex);
+  //       setItemStatus(arr, [minIndex], [counter + 1], [counter]);
+  //       this.setState({
+  //           arr: arr
+  //         },
+  //         () =>
+  //         setTimeout(
+  //           () =>
+  //           algorithm.selectionSort.call(
+  //             this,
+  //             this.state.arr,
+  //             counter + 1,
+  //             counter + 1,
+  //             counter + 1,
+  //             true
+  //           ),
+  //           this.state.speed
+  //         )
+  //       );
+  //     }
+  //   }
+  // },
+  insertionSort: function (arr) {
     for (let i = 1; i < arr.length; i++) {
       for (let j = i; j > 0 && arr[j].height < arr[j - 1].height; j--) {
         setItemStatus(arr, [], [j, j - 1]);
@@ -177,11 +142,11 @@ const algorithm = {
     setItemStatus(arr, [], [], [], true);
     queueAnimation.call(this, arr, true);
   },
-  insertionSortWrapper: function(arr) {
+  insertionSortWrapper: function (arr) {
     algorithm.insertionSort.call(this, arr);
     algorithm.counter = 1;
   },
-  quickSort: function(arr, left, right) {
+  quickSort: function (arr, left, right) {
     if (arr.length > 1) {
       let index = partition.call(this, arr, left, right);
       if (left < index - 1) {
@@ -193,14 +158,14 @@ const algorithm = {
     }
     return arr;
   },
-  quickSortWrapper: function(arr, left, right) {
+  quickSortWrapper: function (arr, left, right) {
     algorithm.stateArr = JSON.parse(JSON.stringify(arr));
     algorithm.quickSort.call(this, arr, left, right);
     arr.map(item => (item.status = "sorted"));
     queueAnimation.call(this, arr, true);
     algorithm.counter = 1;
   },
-  mergeSort: function(arr) {
+  mergeSort: function (arr) {
     if (arr.length <= 1) {
       return arr;
     }
@@ -211,7 +176,7 @@ const algorithm = {
     arr = merge.call(this, left, right);
     return arr;
   },
-  mergeSortWrapper: function(arr) {
+  mergeSortWrapper: function (arr) {
     algorithm.stateArr = JSON.parse(JSON.stringify(arr));
     algorithm.mergeSort.call(this, arr);
     algorithm.counter = 1;
@@ -323,7 +288,7 @@ function merge(left, right, sortedArray = []) {
 }
 
 function animateArraySorting(sortedArray) {
-  let minimum = findMinimum(algorithm.stateArr, sortedArray);
+  let minimum = findMinimumIndex(algorithm.stateArr, sortedArray);
   for (let i = 0; i < sortedArray.length; i++) {
     algorithm.stateArr[minimum + i] = JSON.parse(
       JSON.stringify(sortedArray[i])
@@ -332,7 +297,7 @@ function animateArraySorting(sortedArray) {
   }
 }
 
-function findMinimum(unsortedArr, sortedArray) {
+function findMinimumIndex(unsortedArr, sortedArray) {
   for (let i = 0; i < unsortedArr.length; i++) {
     for (let j = 0; j < sortedArray.length; j++) {
       if (unsortedArr[i].index === sortedArray[j].index) {
